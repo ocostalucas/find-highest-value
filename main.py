c56c0c01ebea2a4e0483886a6815118065f7e64f
ocostalucas <olucascosta98@gmail.com>
2020-08-15 20:53:36 -0300

    Add output function

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ def extractor(filename):
     archive.close()
     return [int(i) for i in data]
 
+def output(filename,larger, time):
+    archive = open('resposta-'+filename+'.txt','w')
+    archive.write(larger)
+    archive.write(time)
+    archive.close() 
+
 def program(filename):
     start = time.time()
     content = extractor(filename+'.csv')
     larger = max(i for i in content)
-    finish=str((time.time() - start)*1000) 
-    return larger 
+    finish=str((time.time() - start)*1000)
+    output(filename, str(larger) + '\n', str(finish)) 
 
-print(program('dataset-2-b'))
+program('dataset-2-a')
+program('dataset-2-b')
+program('dataset-2-c')
+program('dataset-2-d')
+program('dataset-2-e')
